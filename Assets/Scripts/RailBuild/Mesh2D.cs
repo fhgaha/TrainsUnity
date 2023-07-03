a5c0a2ee1e6190ebb37a758b7bdf20134776f2d8
fhgaha <antsy1992@gmail.com>
2023-07-03 20:03:25 +0300

    new road 2dShape

--- Assets/Scripts/RailBuild/Mesh2D.cs
+++ Assets/Scripts/RailBuild/Mesh2D.cs
@@ -28,7 +28,7 @@ namespace Trains
 		|__________|
 		*/
 
-		public List<Vertex> testPointSet = new()
+		public List<Vertex> testVertexSet = new()
 		{
 			new Vertex{point = new Vector2(3, 0),   normal = new Vector2(0, 1),                             u = 0.171f},
 			new Vertex{point = new Vector2(3, 0),   normal = new Vector2(-0.70710678118f, 0.70710678118f),  u = 0.171f},
@@ -48,8 +48,29 @@ namespace Trains
 			new Vertex{point = new Vector2(-3, 0),  normal = new Vector2(0, 1),                             u = 0.403f},
 		};
 
-		public List<int> testIndexSet = new()
-		{15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+		public List<int> testIndexSet = new() { 15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+
+		/*
+		  _______		  
+		 /		 \
+		/_________\
+		*/
+		public List<Vertex> trapezoidVertexSet = new()
+		{
+			CreateVertex(new Vector2(3, 0),     new Vector2(0, 1),                              0.1f),
+			CreateVertex(new Vector2(3, 0),     new Vector2(0.70710678118f, 0.70710678118f),    0.1f),
+			CreateVertex(new Vector2(5, -2),    new Vector2(0.70710678118f, 0.70710678118f),    0f),
+			CreateVertex(new Vector2(5, -2),    new Vector2(0, -1),                             1f),
+			CreateVertex(new Vector2(-5, -2),   new Vector2(0, -1),                             0.6f),
+			CreateVertex(new Vector2(-5, -2),   new Vector2(-0.70710678118f, 0.70710678118f),   0.6f),
+			CreateVertex(new Vector2(-3, 0),    new Vector2(-0.70710678118f, 0.70710678118f),   0.5f),
+			CreateVertex(new Vector2(-3, 0),    new Vector2(0, 1),                              0.5f),
+		};
+
+		public List<int> trapezoidIndexSet = new() { 7, 0, 1, 2, 3, 4, 5, 6 };
+
+
 
 		public float CalcUspan()
 		{
